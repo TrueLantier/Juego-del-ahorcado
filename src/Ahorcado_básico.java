9a315fd894e523265a07414154483c9fe0bb17c7
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-11-18 14:57:21 -0500

    Tomando forma lo básico.

--- src/Ahorcado_básico.java
+++ src/Ahorcado_básico.java
@@ -1,5 +1,5 @@
+// Métodos propios. Luego usaré StringBuild y sus métodos.
 import java.util.Scanner;
-// Métodos míos. Luego usaré métodos hechos.
 
 public class Ahorcado_básico {
     public static void main(String[] args) {
@@ -9,38 +9,49 @@ public class Ahorcado_básico {
         String[] lista = {"angel"};
 
         String palabra = lista[0];
+        char[] caracteres = new char[palabra.length()];
+        for ( int i = 0; i < palabra.length(); i++) {
+            caracteres[i] = '_';
+        }
+        String palabra_formada = "";
         char ingreso;
         boolean util = true;
         boolean encontrar_letra = false;
         int errores_max = palabra.length();
         int contador_errores = 0;
+        int contador_aciertos = 0;
 
         do {
             for ( int i = 0; i < palabra.length(); i++) {
-                System.out.print("_");
+                System.out.print( caracteres[i] );
             }
 
-            System.out.println("Ingrese una letra: ");
+            System.out.println("\nIngrese una letra: ");
             ingreso = scanner.nextLine().charAt(0);
 
             for ( int i = 0; i < palabra.length(); i++) {
                 if ( ingreso == palabra.charAt(i) ) {
+                    caracteres[i] = ingreso;
                     encontrar_letra = true;
+                    contador_aciertos++;
                     break;
+                }   else {
+                    encontrar_letra = false;
                 }
             }
-            if (encontrar_letra) {
 
+            if ( !encontrar_letra ) {
+                System.out.println("Letra incorrecta.");
+                contador_errores++;
             }
 
-            /*
-            util = false;
-                    contador_errores++;
-             */
+        }   while ( (contador_errores < palabra.length()) || (contador_aciertos < palabra.length()) );
 
-        }   while ( contador_errores < errores_max );
+        for ( int i = 0; i < palabra.length(); i++) {
+            palabra_formada += caracteres[i];
+        }
+        System.out.println(palabra_formada);
 
-
-        //scanner.close();
+        scanner.close();
     }
 }
